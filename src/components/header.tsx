d1ec9ad7a9d4767b5f876434690f531b1df17261
hitman077 <ntw3840@gmail.com>
2025-08-06 08:56:32 +0700

    Your commit message here

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -103,6 +103,20 @@ export const HeroHeader = () => {
 									{/* <NavigationMenus /> */}
 								</ul>
 							</div>
+							<div className="flex items-center gap-4 lg:block">
+								{/* <ModeToggle /> */}
+								<Button
+									variant="ghost"
+									size="icon"
+									onClick={toggleTheme}
+								>
+									<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+									<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+									<span className="sr-only">
+										Toggle theme
+									</span>
+								</Button>
+							</div>
 							<div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
 								<Button asChild variant="outline" size="sm">
 									<Link href="#">
